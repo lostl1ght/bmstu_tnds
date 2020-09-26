dc7d65cee65fea92b2b00b8c8aa79e6b915ac16e
Lostlight <robpirojk@gmail.com>
2020-09-26 19:50:49 +0300

    Checks mantissa

--- lab_01/inc/check.h
+++ lab_01/inc/check.h
@@ -6,7 +6,10 @@
 int check_str(char str[]);
 int check_char(char *p);
 int check_e(char *p, char **e_pointer);
-int check_mantissa(char *str, char *e_pointer);
+size_t count_char(char c, char *start, char *end);
+size_t count_num(char *start, char *end);
+int check_symb(char *start, char *end, size_t dot_count);
+int check_mantissa(char *str, char *end);
 int check_exponent(char *e_pointer);
 int check_exp_len(char *e_pointer);
 int check_len_exp_num(char *p);
--- lab_01/src/check.c
+++ lab_01/src/check.c
@@ -15,8 +15,8 @@ int check_str(char str[])
         return CHECK_WRONG_CHARACTERS;
     if (check_e(str, &e_pointer))
         return CHECK_WRONG_CHARACTERS;
-    // if (check_mantissa(str, e_pointer))
-    //     return CHECK_MANT_ERROR;
+    if (check_mantissa(str, e_pointer))
+        return CHECK_MANT_ERROR;
     if (e_pointer && check_exponent(e_pointer))
         return CHECK_EXP_ERROR;
     return CHECK_SUCCESS;
@@ -24,11 +24,12 @@ int check_str(char str[])
 
 int check_char(char *p)
 {
-    char s;
+    char s[2];
+    s[1] = '\0';
     while (*p)
     {
-        s = *p;
-        if (!strpbrk("-+.,1234567890Ee\n", &s))
+        s[0] = *p;
+        if (!strpbrk("-+.,1234567890Ee\n", s))
             return CHECK_FAILURE;
         p++;
     }
@@ -52,10 +53,66 @@ int check_e(char *p, char **e_pointer)
     return CHECK_SUCCESS;
 }
 
-// int check_mantissa(char *str, char *e_pointer)
-// {
-//     return CHECK_SUCCESS;
-// }
+size_t count_char(char c, char *start, char *end)
+{
+    size_t count = 0;
+    while (start < end)
+    {
+        if (*start == c)
+            count++;
+        start++;
+    }
+    return count;
+}
+
+size_t count_num(char *start, char *end)
+{
+    size_t count = 0;
+    char s[2];
+    s[1] = '\0';
+    while (start < end)
+    {
+        s[0] = *start;
+        if (strpbrk("1234567890", s))
+            count++;
+        start++;
+    }
+    return count;
+}
+
+int check_symb(char *start, char *end, size_t dot_count)
+{
+    size_t count;
+    size_t count_dt, count_cm;
+    count = count_char('-', start, end);
+    if (count > 1)
+        return CHECK_FAILURE;
+    else if (count == 1 && *start != '-')
+        return CHECK_FAILURE;
+    count = count_char('+', start, end);
+    if (count > 1)
+        return CHECK_FAILURE;
+    else if (count == 1 && *start != '+')
+        return CHECK_FAILURE;
+    if ((count_dt = count_char('.', start, end)) > dot_count)
+        return CHECK_FAILURE;
+    if ((count_cm = count_char(',', start, end)) > dot_count)
+        return CHECK_FAILURE;
+    if (count_cm && count_dt)
+        return CHECK_FAILURE;
+    return CHECK_SUCCESS;
+}
+
+int check_mantissa(char *str, char *end)
+{
+    if (!end)
+        end = str + strlen(str);
+    if (check_symb(str, end, 1))
+        return CHECK_FAILURE;
+    if (count_num(str, end) > MAX_MANTISSA)
+        return CHECK_FAILURE;
+    return CHECK_SUCCESS;
+}
 
 int check_exponent(char *e_pointer)
 {
@@ -94,9 +151,8 @@ int check_exp_len(char *e_pointer)
                 return CHECK_FAILURE;
         }
     }
-    else
-        if (check_len_exp_num(e_pointer))
-            return CHECK_FAILURE;
+    else if (check_len_exp_num(e_pointer))
+        return CHECK_FAILURE;
     return CHECK_SUCCESS;
 }
 
