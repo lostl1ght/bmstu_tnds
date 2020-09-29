f658063aecb27a92ebbb6a2bc4393b2728ffbd58
Lostlight <robpirojk@gmail.com>
2020-09-29 12:55:00 +0300

    Num update

--- lab_01/inc/number.h
+++ lab_01/inc/number.h
@@ -5,13 +5,13 @@
 
 #define MAX_MANTISSA 5
 #define MAX_EXPONENT 2
-// 5 in mantissa + 2 in exponent  + (2 signs + . + E + "\n" + "\0" = 7) + 3 to check
+// mantissa + exponent  + (2 signs + . + E + "\n" + "\0" = 7) + 3 to check
 #define LEN MAX_MANTISSA + MAX_EXPONENT + 7 + 3
 
 typedef struct number
 {
     int sign_m;
-    int mantissa[MAX_MANTISSA * 2];
+    int mantissa[MAX_MANTISSA];
     size_t len_m;
     int exponent;
 } number_t;
