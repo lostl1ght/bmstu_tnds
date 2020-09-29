2ded7ac366acea87b36b9f8c641f5e2ceb9d1631
Lostlight <robpirojk@gmail.com>
2020-09-29 18:38:51 +0300

    Mantissa mult

--- lab_01/inc/multiply.h
+++ lab_01/inc/multiply.h
@@ -1,6 +1,8 @@
 #ifndef MULTIPLY_H
 #define MULTIPLY_H
 
+#include "../inc/number.h"
 
+void multiply_mant(number_t *num1, number_t *num2, int arr0[]);
 
 #endif
--- lab_01/src/multiply.c
+++ lab_01/src/multiply.c
@@ -1,3 +1,37 @@
 #include "../inc/multiply.h"
-#include "../inc/number.h"
 
+void multiply_mant(number_t *num1, number_t *num2, int arr0[])
+{
+    int arr1[MAX_MANTISSA * 2] = {0};
+    if (num2->len_m > num1->len_m)
+    {
+        number_t *tmp= num1;
+        num1 = num2;
+        num2 = tmp;
+    }
+
+    for (size_t j = 0; j < num1->len_m; j++)
+        arr0[j] = num1->mantissa[j] * num2->mantissa[0];
+
+    for (size_t j = 0; j < num1->len_m; j++)
+    {
+        arr0[j + 1] += arr0[j] / 10;
+        arr0[j] %= 10;
+    }
+
+    for (size_t i = 1; i < num2->len_m; i++)
+    {
+        for (size_t j = 0; j < num1->len_m; j++)
+            arr1[j + i] = num1->mantissa[j] * num2->mantissa[i];
+        for (size_t j = 0; j < num1->len_m + i; j++)
+            arr0[j] += arr1[j];
+        for (size_t j = 0; j < num1->len_m + i; j++)
+            arr1[j] = 0;
+        
+        for (size_t j = 0; j < num1->len_m + i; j++)
+        {
+            arr0[j + 1] += arr0[j] / 10;
+            arr0[j] %= 10;
+        }
+    }
+}
