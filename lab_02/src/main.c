910a79e4da34606b46117ac75756e3dd6434ea6a
Lostlight <robpirojk@gmail.com>
2020-10-07 20:19:12 +0300

    Main

--- lab_02/src/main.c
+++ lab_02/src/main.c
@@ -1,18 +1,27 @@
 #define _USE_MINGW_ANSI_STDIO 1
 #include "flat.h"
 
+#define SUCCESS 0
+#define COUNT_FAILURE 1
+#define READ_FAILURE 2
 
-
-int main(void) //int argc, char **argv
+int main(void)
 {
-    flat_t *flats = NULL;
-    size_t flt_cnt = 0;
+    flat_t *flats;
+    int flt_cnt = 0;
     char buf;
     printf("Enter number of flats: ");
-    if (scanf("%zu%c", &flt_cnt, &buf) != 2)
-        return 1;
-    if (input_flat_array(flats, flt_cnt))
-        return 1;
-    
-    return 0;
+    if (scanf("%d%c", &flt_cnt, &buf) != 2 || flt_cnt < 0)
+    {
+        printf("Wrong number of flats!");
+        return COUNT_FAILURE;
+    }
+    if (input_flat_array(&flats, flt_cnt))
+    {
+        printf("Wrong input!");
+        return READ_FAILURE;
+    }
+    output_flat_array(flats, flt_cnt);
+    free(flats);
+    return SUCCESS;
 }
