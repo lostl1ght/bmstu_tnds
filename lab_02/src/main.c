775e2f72eba16ee029e2a5e31e983fae06a63112
Lostlight <robpirojk@gmail.com>
2020-10-11 20:14:00 +0300

    Help added

--- lab_02/src/main.c
+++ lab_02/src/main.c
@@ -9,16 +9,18 @@
 #define ARG_FAILURE 3
 #define OPEN_FAILURE 4
 
+void help(void);
+
 int main(int argc, char **argv)
 {
     if (argc < 2 || argc == 4 || argc > 5)
-    {   
+    {
         puts("Unknown arguments.");
         return ARG_FAILURE;
     }
     else if (argc == 2 && strcmp(argv[1], "-help") == 0)
     {
-        //Вывести справку
+        help();
     }
     else if (argc == 3 && strcmp(argv[1], "-i") == 0)
     {
@@ -38,4 +40,13 @@ int main(int argc, char **argv)
     }
 
     return SUCCESS;
+}
+
+void help(void)
+{
+    puts("-help | Get help.");
+    puts("-s1 [k] in.txt out.txt | Sort 1 struct array from in.txt and write to out.txt. If k is invoked, array of keys will be sorted instead.");
+    puts("-s2 [k] in.txt out.txt | Sort 2 struct array from in.txt and write to out.txt. If k is invoked, array of keys will be sorted instead.");
+    puts("-d index in.txt out.txt | Delete index flat from in.txt and write the rest to out.txt");
+    puts("-o in.txt | Output a table of flats from in.txt.");
 }
