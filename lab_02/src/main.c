d19ece7d5e848a2c8c30e6303f5124e2993a13f0
Lostlight <robpirojk@gmail.com>
2020-10-11 20:17:21 +0300

    Minor help fix

--- lab_02/src/main.c
+++ lab_02/src/main.c
@@ -45,8 +45,9 @@ int main(int argc, char **argv)
 void help(void)
 {
     puts("-help | Get help.");
+    puts("-i out.txt | Get flats from stream and write them to out.txt");
+    puts("-o in.txt | Output a table of flats from in.txt.");
     puts("-s1 [k] in.txt out.txt | Sort 1 struct array from in.txt and write to out.txt. If k is invoked, array of keys will be sorted instead.");
     puts("-s2 [k] in.txt out.txt | Sort 2 struct array from in.txt and write to out.txt. If k is invoked, array of keys will be sorted instead.");
-    puts("-d index in.txt out.txt | Delete index flat from in.txt and write the rest to out.txt");
-    puts("-o in.txt | Output a table of flats from in.txt.");
+    puts("-d index in.txt out.txt | Delete an index flat from in.txt and write the rest to out.txt");
 }
