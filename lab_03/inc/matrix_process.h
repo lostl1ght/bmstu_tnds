0478b477dd98bf35a058c040019773d3d8849270
lostlight <robpirojk@gmail.com>
2020-11-14 00:09:53 +0300

    Convert initial

---
+++ lab_03/inc/matrix_process.h
@@ -0,0 +1,9 @@
+#ifndef MATRIX_PROCESS_H
+#define MATRIX_PROCESS_H
+
+#include "matrix.h"
+#include "sparse.h"
+
+void convert_to_sparse(matrix_s *m, sparse_s *s);
+
+#endif
---
+++ lab_03/src/matrix_process.c
@@ -0,0 +1,6 @@
+#include "matrix_process.h"
+
+void convert_to_sparse(matrix_s *m, sparse_s *s)
+{
+    
+}
