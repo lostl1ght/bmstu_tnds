f78cc898caba2f5690c0383e997ddef318efecf9
lostlight <robpirojk@gmail.com>
2020-11-15 23:26:33 +0300

    Non zero count func

--- lab_03/inc/matrix_process.h
+++ lab_03/inc/matrix_process.h
@@ -5,5 +5,6 @@
 #include "sparse.h"
 
 void convert_to_sparse(matrix_s *m, sparse_s *s);
+stype_t non_zero_count(matrix_s *m);
 
 #endif
--- lab_03/src/matrix_process.c
+++ lab_03/src/matrix_process.c
@@ -2,5 +2,16 @@
 
 void convert_to_sparse(matrix_s *m, sparse_s *s)
 {
-    
+    s->n_count = non_zero_count(m);
+    s->c_count = m->cols;
+}
+
+stype_t non_zero_count(matrix_s *m)
+{
+    stype_t count = 0;
+    for (stype_t j = 0; j < m->cols; j++)
+        for (stype_t i = 0; i < m->rows; i++)
+            if (m->matrix[j][i] != 0)
+                count++;
+    return count;
 }
