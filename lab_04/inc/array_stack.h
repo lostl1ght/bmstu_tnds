966d1037a5a9d24f0fcf86a7b0042e91a1c55bbe
lostlight <robpirojk@gmail.com>
2020-12-09 01:36:22 +0300

    Add and delete arrstack

--- lab_04/inc/array_stack.h
+++ lab_04/inc/array_stack.h
@@ -7,20 +7,20 @@
 typedef struct arrstack
 {
     double *arr;
-    double *cur1;
+    double *top1;
     double *end;
-    double *cur2;
+    double *top2;
 } arrstack_t;
 
 arrstack_t *create_arrstack(const size_t size);
 void delete_arrstack(arrstack_t *stack);
 void output_arrstack(arrstack_t stack);
 
-int add_1st_stack(arrstack_t *stack, double num);
-void pop_1st_stack(arrstack_t *stack);
-void empty_1st_stack(arrstack_t *stack);
-int add_2nd_stack(arrstack_t *stack, double num);
-void pop_2st_stack(arrstack_t *stack);
-void empty_2nd_stack(arrstack_t *stack);
+int add_1st_arrstack(arrstack_t *stack, double num);
+int pop_1st_arrstack(arrstack_t *stack, double *num);
+void empty_1st_arrstack(arrstack_t *stack);
+int add_2nd_arrstack(arrstack_t *stack, double num);
+int pop_2st_arrstack(arrstack_t *stack, double *num);
+void empty_2nd_arrstack(arrstack_t *stack);
 
 #endif
--- lab_04/src/array_stack.c
+++ lab_04/src/array_stack.c
@@ -11,8 +11,8 @@ arrstack_t *create_arrstack(const size_t size)
         return NULL;
     }
     stack->end = stack->arr + size;
-    stack->cur1 = stack->arr;
-    stack->cur2 = stack->end;
+    stack->top1 = stack->arr;
+    stack->top2 = stack->end;
     return stack;
 }
 
@@ -25,22 +25,60 @@ void delete_arrstack(arrstack_t *stack)
 void output_arrstack(arrstack_t stack)
 {
     puts("First array stack:");
-    while (stack.arr <= stack.cur1)
+    while (stack.arr <= stack.top1)
     {
-        printf("%lf\n", stack.cur1);
-        stack.cur1--;
+        printf("%lf\n", stack.top1);
+        stack.top1--;
     }
     puts("Second stack:");
-    while (stack.end > stack.cur2)
+    while (stack.end > stack.top2)
     {
-        printf("%lf\n", stack.cur2);
-        stack.cur2++;
+        printf("%lf\n", stack.top2);
+        stack.top2++;
     }
 }
 
-int add_1st_stack(arrstack_t *stack, double num);
-void pop_1st_stack(arrstack_t *stack);
-void empty_1st_stack(arrstack_t *stack);
-int add_2nd_stack(arrstack_t *stack, double num);
-void pop_2st_stack(arrstack_t *stack);
-void empty_2nd_stack(arrstack_t *stack);
+int add_1st_arrstack(arrstack_t *stack, double num)
+{
+    if (stack->top1 + 1 == stack->top2)
+        return 1;
+    *stack->top1 = num;
+    stack->top1++;
+    return 0;
+}
+
+int pop_1st_arrstack(arrstack_t *stack, double *num)
+{
+    if (stack->top1 == stack->arr)
+    stack->top1--;
+    *num = *stack->top1;
+    return 0;
+}
+
+void empty_1st_arrstack(arrstack_t *stack)
+{
+    stack->top1 = stack->arr;
+}
+
+int add_2nd_arrstack(arrstack_t *stack, double num)
+{
+    if (stack->top1 + 1 == stack->top2)
+        return 1;
+    stack->top2--;
+    *stack->top2 = num;
+    return 0;
+}
+
+int pop_2st_arrstack(arrstack_t *stack, double *num)
+{
+    if (stack->top2 == stack->end)
+        return 1;
+    *num = *stack->top2;
+    stack->top2++;
+    return 0;
+}
+
+void empty_2nd_arrstack(arrstack_t *stack)
+{
+    stack->top2 = stack->end;
+}
