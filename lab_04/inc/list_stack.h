330bf739b08d073eb2ac51323e237692d487167e
lostlight <robpirojk@gmail.com>
2020-12-10 00:28:55 +0300

    Time added

--- lab_04/inc/list_stack.h
+++ lab_04/inc/list_stack.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "tick.h"
 
 typedef struct stacknode stacknode_t;
 
@@ -19,8 +20,8 @@ void output_stack_node(const stacknode_t *node);
 void delete_list_stack(stacknode_t *top);
 void output_list_stack(const stacknode_t *top);
 
-stacknode_t *push_list_stack(stacknode_t *top, const double num);
-int pop_list_stack(stacknode_t **top, double *num);
-stacknode_t *empty_list_stack(stacknode_t *top);
+stacknode_t *push_list_stack(stacknode_t *top, const double num, uint64_t *ticks);
+int pop_list_stack(stacknode_t **top, double *num, uint64_t *ticks);
+stacknode_t *empty_list_stack(stacknode_t *top, uint64_t *ticks);
 
 #endif
--- lab_04/src/list_stack.c
+++ lab_04/src/list_stack.c
@@ -38,33 +38,47 @@ void output_list_stack(const stacknode_t *top)
     }
 }
 
-stacknode_t *push_list_stack(stacknode_t *top, const double num)
+stacknode_t *push_list_stack(stacknode_t *top, const double num, uint64_t *ticks)
 {
     stacknode_t *node;
+    uint64_t start, end;
+    start = tick();
     if (!(node = create_stack_node(num)))
         return NULL;
-    // Вывод адреса
     node->next = top;
+    end = tick();
+    *ticks = end - start;
+    // Вывод адреса;
     return node;
 }
 
-int pop_list_stack(stacknode_t **top, double *num)
+int pop_list_stack(stacknode_t **top, double *num, uint64_t *ticks)
 {
     if (*top)
     {
+        void *adr;
+        uint64_t start, end;
+        start = tick();
         stacknode_t *new_top = (*top)->next;
         *num = (*top)->num;
-        // Запись адреса
+        adr = *top;
         delete_stack_node(*top);
         *top = new_top;
+        end = tick();
+        *ticks = end - start;
+        // Запись адреса
         return 0;
     }
     else
         return 1;
 }
 
-stacknode_t *empty_list_stack(stacknode_t *top)
+stacknode_t *empty_list_stack(stacknode_t *top, uint64_t *ticks)
 {
+    uint64_t start, end;
+    start = tick();
     delete_list_stack(top);
+    end = tick();
+    *ticks = end - start;
     return NULL;
 }
