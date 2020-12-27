baaec24080edac42b0b70525bd1b386a006ff0ce
lostlight <robpirojk@gmail.com>
2020-12-27 21:31:31 +0300

    Interface remade

--- lab_06/src/main.c
+++ lab_06/src/main.c
@@ -1,26 +1,24 @@
 #include "tree_utils.h"
 #include "list_utils.h"
 #include "hash_utils.h"
-#include "search.h"
+#include "search_utils.h"
 #include "tick.h"
 
 int main(int argc, char *argv[])
 {
-    welcome();
+    printf("Trees, hash and whatever...\n");
 
     FILE *f = fopen(argv[1], "r");
 
     if (f == NULL || argc != 2)
     {
-        printf(
-            "Неверное имя файла! Повторите попытку.\n");
+        printf("Wrong file.\n");
         return FILE_ERROR;
     }
 
     if (fgetc(f) == EOF)
     {
-        printf(
-            "Файл пуст! Проверьте файл.\n");
+        printf("File is empty\n");
         return FILE_ERROR;
     }
 
@@ -34,24 +32,17 @@ int main(int argc, char *argv[])
     fill_tree(&root, f);
     time = tick() - time;
 
-    printf(
-        "БИНАРНОЕ ДЕРЕВО НА ОСНОВЕ ДАННЫХ ИЗ ФАЙЛА:\n");
+    printf("Binary tree from files:\n");
     print_tree(root, 0);
-    printf(
-        "Дерево построено за = %ld тактов процессора "
-        "(в данное время включено время чтения из файла).\n\n",
-        time);
+    printf("Tree building time: %ld\n\n", time);
 
     int unique;
     time = tick();
     root = balance_tree(root, &unique);
     time = tick() - time;
-    printf(
-        "БИНАРНОЕ ДЕРЕВО ПОСЛЕ БАЛАНСИРОВКИ:\n");
+    printf("Balanced binary tree:\n");
     print_tree(root, 0);
-    printf(
-        "Дерево сбалансировано за = %ld тактов процессора.\n\n",
-        time);
+    printf("Tree balancing time: %ld\n\n", time);
 
     int n = next_prime(file_len(f));
     list_t *hash_list = calloc(n, sizeof(list_t));
@@ -62,39 +53,28 @@ int main(int argc, char *argv[])
     time = tick();
     unsafe_hash("chill", 10);
     time = tick() - time;
-    printf(
-        "%ld - генерация простой хеш-функции (в тактах процессора).\n",
-        time);
+    printf("Simple hash generating time: %ld.\n", time);
 
     time = tick();
     safe_hash("chill", 10);
     time = tick() - time;
-    printf(
-        "%ld - генерация сложной хеш-функции (в тактах процессора).\n",
-        time);
+    printf("Better hash generating time: %ld\n", time);
 
     hash = &unsafe_hash;
     time = tick();
     cmpr = build_hash_table(&hash_list, n, f, hash);
     time = tick() - time;
 
-    printf(
-        "\nХЕШ-ТАБЛИЦА НА ОСНОВЕ ПРОСТОЙ ФУНКЦИИ:");
+    printf("\nSimple hash table:");
     print_hash_table(hash_list, n);
-    printf(
-        "Максимальное количество коллизий = %d.\n",
-        cmpr);
-    printf(
-        "Хеш-таблица построена за = %ld тактов процессора "
-        "(в данное время включено время чтения из файла).\n\n",
-        time);
+    printf("Max clash count: %d\n", cmpr);
+    printf("Hash table building time: %ld\n\n", time);
 
-    printf("\nВведите допустимое количество коллизий: ");
+    printf("\nEnter max clash count: ");
     int ec = scanf("%d", &desired_cmpr);
-    if (ec != 1)
+    if (ec != 1 || desired_cmpr < 1)
     {
-        printf("Введено неверное значение!\n");
-
+        printf("Wrong number.\n");
         return VAL_ERROR;
     }
 
@@ -112,25 +92,18 @@ int main(int argc, char *argv[])
             time = tick() - time;
         }
         printf(
-            "\nХЕШ-ТАБЛИЦА НА ОСНОВЕ УЛУЧШЕННОЙ ФУНКЦИИ:");
+            "\nBetter hash table:");
         print_hash_table(hash_list, n);
-        printf(
-            "Количество коллизий = %d.\n",
-            cmpr);
-        printf(
-            "Хеш-таблица построена за = %ld тактов процессора "
-            "(в данное время включено время чтения из файла).\n\n",
-            time);
+        printf("Max clash count: %d\n", cmpr);
+        printf("Hash table building time: %ld\n\n", time);
     }
     else
     {
-        printf(
-            "Результат достижим за введенное количество коллизий.\n"
-            "Пересоздание таблицы не требуется.\n");
+        printf("No need to remake the table.");
     }
 
     char to_find[STR_SIZE];
-    printf("\nВведите слово которое хотите найти: ");
+    printf("\nEnter a word to find: ");
     scanf("%s", to_find);
 
     int depth = 0;
@@ -144,22 +117,19 @@ int main(int argc, char *argv[])
     tree_depth(root2, &vrtxs, &cmprs, depth);
     if (ec > 0)
     {
-        printf("ПОИСК В БИНАРНОМ ДЕРЕВЕ\n");
-        printf("Слово \"%s\" найдено за %ld тактов процессора.\n"
-               "Среднее время поиска в бинарном дереве - %lf тактов процессора.\n"
-               "Бинарное дерево занимает - %ld байт.\n"
-               "Количество сравнений для достижения результата - %d.\n"
-               "Среднее количество сравнений - %f.\n",
+        printf("\nBinary tree search\n");
+        printf("Word \"%s\" searching time: %ld\n"
+               "Average searching time: %lf\n"
+               "Memory size: %ld\n"
+               "Comapres count: %d\n"
+               "Average comapres count: %f.\n",
                to_find, time, search_tree_av(*root2, words, flen),
                unique * sizeof(tree_node), ec,
                (double)cmprs / vrtxs);
     }
     else
     {
-        printf(
-            "Слово \"%s\" не найдено.\n",
-            to_find);
-
+        printf("Word \"%s\" wasn't found.\n", to_find);
         return VAL_ERROR;
     }
     free_tree(root2);
@@ -173,22 +143,19 @@ int main(int argc, char *argv[])
     tree_depth(root, &vrtxs, &cmprs, depth);
     if (ec > 0)
     {
-        printf("ПОИСК В БИНАРНОМ СБАЛАНСИРОВАННОМ ДЕРЕВЕ\n");
-        printf("Слово \"%s\" найдено за %ld тактов процессора.\n"
-               "Среднее время поиска в бинарном сбалансированном дереве - %lf тактов процессора.\n"
-               "Бинарное сбалансированное дерево занимает - %ld байт.\n"
-               "Количество сравнений для достижения результата - %d.\n"
-               "Среднее количество сравнений - %f.\n",
+        printf("\nBalanced binaty tree search\n");
+        printf("Word \"%s\" searching time: %ld\n"
+               "Average searching time: %lf\n"
+               "Memory size: %ld\n"
+               "Comapres count: %d\n"
+               "Average comapres count: %f.\n",
                to_find, time, search_tree_av(*root, words, flen),
                unique * sizeof(tree_node), ec,
                (double)cmprs / vrtxs);
     }
     else
     {
-        printf(
-            "Слово \"%s\" не найдено.\n",
-            to_find);
-
+        printf("Word \"%s\" wasn't found.\n", to_find);
         return VAL_ERROR;
     }
     free_tree(root);
@@ -198,22 +165,19 @@ int main(int argc, char *argv[])
     time = tick() - time;
     if (ec > 0)
     {
-        printf("ПОИСК В ХЕШ-ТАБЛИЦЕ\n");
-        printf("Слово \"%s\" найдено за %ld тактов процессора.\n"
-               "Среднее время поиска в хеш-таблице - %lf тактов процессора.\n"
-               "Хеш-таблица занимает - %ld байт.\n"
-               "Количество сравнений для достижения результата - %d.\n"
-               "Среднее количество сравнений - %f.\n",
+        printf("\nHash table search\n");
+        printf("Word \"%s\" searching time: %ld\n"
+               "Average searching time: %lf\n"
+               "Memory size: %ld\n"
+               "Comapres count: %d\n"
+               "Average comapres count: %f.\n",
                to_find, time / 4, search_hashtable_av(hash_list, n, hash, words, flen),
                (n + unique - list_occupation(hash_list, n)) * sizeof(list_t) + sizeof(list_t *), ec,
                (double)(1 + desired_cmpr) / 2);
     }
     else
     {
-        printf(
-            "Слово \"%s\" не найдено.\n",
-            to_find);
-
+        printf("Word \"%s\" wasn't found.\n", to_find);
         return VAL_ERROR;
     }
     free_list_arr(hash_list, n);
@@ -223,21 +187,18 @@ int main(int argc, char *argv[])
     time = tick() - time;
     if (ec > 0)
     {
-        printf("ПОИСК В ФАЙЛЕ\n");
-        printf("Слово \"%s\" найдено за %ld тактов процессора.\n"
-               "Среднее время поиска в файле - %lf тактов процессора.\n"
-               "Файл занимает - %u байт.\n"
-               "Количество сравнений для достижения результата - %d.\n"
-               "Среднее количество сравнений - %f.\n",
+        printf("\nFile search\n");
+        printf("Word \"%s\" searching time: %ld\n"
+               "Average searching time: %lf\n"
+               "Memory size: %u\n"
+               "Comapres count: %d\n"
+               "Average comapres count: %f.\n",
                to_find, time, search_file_av(f, words, flen),
                fbytes(f), ec, (double)file_len(f) / 2);
     }
     else
     {
-        printf(
-            "Слово \"%s\" не найдено.\n",
-            to_find);
-
+        printf("Word \"%s\" wasn't found.\n", to_find);
         return VAL_ERROR;
     }
 
